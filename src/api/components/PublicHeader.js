d34010ade8af0be6e5a4b2e6e5366d2b123b235c
MonicaGilgon <monicagg@ufps.edu.co>
2025-04-17 14:08:11 -0500

    publicheader, pendiente las páginas que conforman el header y vincular categorías creadas al desplegable de catálogo

--- src/api/components/PublicHeader.js
+++ src/api/components/PublicHeader.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from '../images/logo.png';
+import logo2 from '../images/logo2.png';
 import { FaShoppingBag, FaUser, FaSearch } from 'react-icons/fa';
 
 const PublicHeader = () => {
   const navigate = useNavigate(); 
+  const [catalogoAbierto, setCatalogoAbierto] = useState(false);
+  const [masAbierto, setMasAbierto] = useState(false);
+
 
   const handleLoginClick = () => {
     navigate('/sign-in');
@@ -13,10 +16,9 @@ const PublicHeader = () => {
   const handleCartClick = () => {
     navigate('/cart');
   };
-
   return (
     <header style={{ 
-      backgroundColor: '#f8b6d2', 
+      backgroundColor: 'white', 
       padding: '1rem 2rem', 
       display: 'flex', 
       alignItems: 'center', 
@@ -24,16 +26,139 @@ const PublicHeader = () => {
       flexWrap: 'wrap',
     }}>
       {/* Logo + Catálogo */}
-      <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1rem', marginLeft: 35}}>
         <img 
-          src={logo} 
+          src={logo2} 
           alt="Douceur Logo" 
-          style={{ width: '70px', height: '70px', borderRadius: '50%' }} 
+          style={{ width: '75px', height: '85px' }} 
         />
 
-        <select style={{ padding: '0.5rem', borderRadius: '5px' }}>
-          <option value="">Catálogo</option>
-        </select>
+<div style={{ position: 'relative', display: 'inline-block' }}>
+          <button
+            onClick={() => setCatalogoAbierto(!catalogoAbierto)}
+            style={{
+              fontWeight: 'bold',
+              padding: '0.5rem 1.5rem',
+              borderRadius: '5px',
+              background: '#ffffff',
+              color: '#000',
+              border: 'none',
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+              gap: '0.5rem',
+              marginLeft: 60
+            }}>
+            Catálogo 
+          </button>
+          {catalogoAbierto && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '100%',
+                left: 0,
+                background: '#fff',
+                border: '1px solid #ddd',
+                borderRadius: '5px',
+                marginTop: '0.5rem',
+                zIndex: 1000,
+                boxShadow: '0px 4px 6px rgba(0,0,0,0.1)',
+              }}>
+              <a
+                href="/catalogo/categoria1"
+                style={{
+                  display: 'block',
+                  padding: '0.5rem 1.5rem',
+                  textDecoration: 'none',
+                  color: '#000',
+                  fontWeight: 'bold',  
+                  whiteSpace: 'nowrap'                
+                }}>
+                categoria1
+              </a>
+              <a
+                href="/catalago/categoria2"
+                style={{
+                  display: 'block',
+                  padding: '0.5rem 1rem',
+                  textDecoration: 'none',
+                  color: '#000',
+                  fontWeight: 'bold',
+                  whiteSpace: 'nowrap'
+                }}>
+                categoria2
+              </a>
+            </div>
+          )}
+        </div>
+        <a href="/nosotros" style={{  marginLeft: 30, fontWeight: 'bold', padding: '0.5rem', borderRadius: '5px', textDecoration: 'none', 
+          color: '#000', cursor: 'pointer' }}>
+          Nosotros
+        </a>
+        <a href="/dudas" style={{ marginLeft: 30, fontWeight: 'bold', padding: '0.5rem', borderRadius: '5px', textDecoration: 'none', 
+          color: '#000', cursor: 'pointer' }}>
+          Dudas
+        </a>
+
+        <div style={{ position: 'relative', display: 'inline-block' }}>
+          <button
+            onClick={() => setMasAbierto(!masAbierto)}
+            style={{
+              fontWeight: 'bold',
+              padding: '0.5rem 1.5rem',
+              borderRadius: '5px',
+              background: '#ffffff',
+              color: '#000',
+              border: 'none',
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+              gap: '0.5rem',
+              marginLeft: 35
+            }}>
+            Más 
+          </button>
+          {masAbierto && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '100%',
+                left: 0,
+                background: '#fff',
+                border: '1px solid #ddd',
+                borderRadius: '5px',
+                marginTop: '0.5rem',
+                zIndex: 1000,
+                boxShadow: '0px 4px 6px rgba(0,0,0,0.1)',
+              }}>
+              <a
+                href="/politica-datos"
+                style={{
+                  display: 'block',
+                  padding: '0.5rem 1.5rem',
+                  textDecoration: 'none',
+                  color: '#000',
+                  fontWeight: 'bold',  
+                  whiteSpace: 'nowrap'                
+                }}>
+                Política de datos
+              </a>
+              <a
+                href="/terminos-y-condiciones"
+                style={{
+                  display: 'block',
+                  padding: '0.5rem 1rem',
+                  textDecoration: 'none',
+                  color: '#000',
+                  fontWeight: 'bold',
+                  whiteSpace: 'nowrap'
+                }}>
+                Términos y condiciones
+              </a>
+            </div>
+          )}
+        </div>
+
       </div>
 
       {/* Navegación derecha */}
@@ -55,8 +180,9 @@ const PublicHeader = () => {
               border: 'none',
               backgroundColor: '#f5f5f5',
               fontSize: '0.9rem',
-              width: '200px',
+              width: '350px',
               outline: 'none',
+              marginRight: 140
             }}
           />
           <FaSearch style={{
@@ -66,19 +192,21 @@ const PublicHeader = () => {
             transform: 'translateY(-50%)',
             color: '#999',
             pointerEvents: 'none',
+            marginRight: 150
           }} />
         </div>
 
         {/* Botón de carrito */}
         <button 
-          onClick={handleCartClick}
+          /* onClick={handleCartClick} */
           style={{ 
             backgroundColor: '#ffb9cb', 
             border: 'none', 
             padding: '0.5rem 0.75rem', 
             borderRadius: '5px', 
             color: 'white',
-            position: 'relative'
+            position: 'relative',
+            marginRight: 15
           }}
         >
           <FaShoppingBag size={20} />
@@ -107,7 +235,8 @@ const PublicHeader = () => {
             padding: '0.5rem 0.75rem',
             display: 'flex',
             alignItems: 'center',
-            color: 'white'
+            color: 'white',
+            marginRight: 30
           }}
         >
           <FaUser style={{ marginRight: '0.5rem' }} />
