891e7fbdb5ca7a6029be44bb3cfc2f199b109aa7
MonicaGilgon <monicagg@ufps.edu.co>
2025-06-06 16:24:18 -0500

    deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { refreshToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/", // URL del backend
+  baseURL: "https://douceurwebback.onrender.com/", // URL del backend
   headers: {
     "Content-Type": "application/json",
   },
